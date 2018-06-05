6959f03051ade5cc549df87ad577853a18f89410
k911 <konradobal@gmail.com>
2018-06-05 21:15:09 +0200

    Basic json /status endpoint

--- image-service/app/run.py
+++ image-service/app/run.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+import datetime
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/status")
+def status():
+    return jsonify(
+        date=datetime.datetime.now().isoformat(),
+        version=0.1,
+    )
